bd5f31d22c1839e96719dcf93eb1b9baef57a8d2
PaulHuebler <paul.huebler@student.tu-freiberg.de>
2021-04-22 17:37:43 +0200

    Teil 1 der ersten Hausaufgabe

--- main.c
+++ main.c
@@ -3,14 +3,35 @@
 
 #include "bitmap.h"
 
+static void manipulate(bitmap_pixel_hsv_t* pixels, uint32_t count)
+{
+		for (uint32_t x = 0; x < count; x++)
+		{
+
+			bitmap_pixel_hsv_t* pix = &pixels[x];
+
+			if ((pix->v + 50) > 255) {
+				pix->v = 255;
+			} 
+			else if ((pix->v + 50) < 0) {
+				pix->v = 0;
+			}
+			else {
+				pix->v = (pix->v + 50);
+			}
+		}	
+}
+
 int main(void)
 {
 	//Read bitmap pixels, convert to HSV:
 	bitmap_pixel_hsv_t* pixels;
 	uint32_t widthPx, heightPx;
 
-	bitmap_error_t error = bitmapReadPixels("test.bmp", (bitmap_pixel_t**)&pixels, &widthPx, &heightPx, BITMAP_COLOR_SPACE_RGB);
+	bitmap_error_t error = bitmapReadPixels("sails.bmp", (bitmap_pixel_t**)&pixels, &widthPx, &heightPx, BITMAP_COLOR_SPACE_HSV);
 	assert(error == BITMAP_ERROR_SUCCESS);
+
+	manipulate(pixels, widthPx * heightPx);
 
 	//Write bitmap pixels, assume HSV in source:
 	bitmap_parameters_t params;
@@ -21,7 +42,7 @@ int main(void)
 	params.colorDepth = BITMAP_COLOR_DEPTH_24;
 	params.compression = BITMAP_COMPRESSION_NONE;
 	params.dibHeaderFormat = BITMAP_DIB_HEADER_INFO;
-	params.colorSpace = BITMAP_COLOR_SPACE_RGB;
+	params.colorSpace = BITMAP_COLOR_SPACE_HSV;
 
 	error = bitmapWritePixels("test.mod.bmp", BITMAP_BOOL_TRUE, &params, (bitmap_pixel_t*)pixels);
 	assert(error == BITMAP_ERROR_SUCCESS);
